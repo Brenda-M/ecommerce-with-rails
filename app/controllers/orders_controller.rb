923ccd077a71bdaf2f3e2bab63aa8ff4b85616e8
brenda-m <brendanjuguna1@gmail.com>
2024-03-14 12:23:36 +0300

    add dummy payment processor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
-  skip_before_action :authorize, only: %i[ new create ]
+  skip_before_action :authorize, only: %i[new create]
   include CurrentCart
-  before_action :set_cart, only: %i[ new create ]
-  before_action :ensure_cart_isnt_empty, only: %i[ new ]
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: %i[new create]
+  before_action :ensure_cart_isnt_empty, only: %i[new]
+  before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders or /orders.json
   def index
@@ -11,8 +11,7 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1 or /orders/1.json
-  def show
-  end
+  def show; end
 
   # GET /orders/new
   def new
@@ -20,8 +19,7 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /orders or /orders.json
   def create
@@ -32,8 +30,9 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
-        format.html { redirect_to store_index_url, notice: "Thank you for your order." }
+        @order.charge!(pay_type_params)
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
+        format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -46,7 +45,7 @@ class OrdersController < ApplicationController
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
+        format.html { redirect_to order_url(@order), notice: 'Order was successfully updated.' }
         format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -60,25 +59,38 @@ class OrdersController < ApplicationController
     @order.destroy!
 
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:name, :address, :email, :pay_type)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
 
-    def ensure_cart_isnt_empty
-            if @cart.line_items.empty?
-                            redirect_to store_index_url, notice: 'Your cart is empty'
-            end
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:name, :address, :email, :pay_type)
+  end
+
+  def ensure_cart_isnt_empty
+    return unless @cart.line_items.empty?
+
+    redirect_to store_index_url, notice: 'Your cart is empty'
+  end
+
+  def pay_type_params
+    if order_params[:pay_type] == 'Credit card'
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == 'Check'
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == 'Purchase order'
+      params.require(:order).permit(:po_number)
+    else
+      {}
     end
+  end
 end
